b89dd1d27f7875b6d1ea81878679b78295d59b71
Peter Lesslie <pclesslie@gmail.com>
2017-02-02 18:21:54 -0600

    Add CGreen test framework

--- gram.y
+++ gram.y
@@ -1,33 +1,52 @@
 %include {
+
 #include <stdint.h>
 #include <assert.h>
 
-struct ParseTree {
-    // TODO(plesslie): hold result of a parse
-    int dummy;
+typedef void SchemeParser;
+
+enum NodeType {
+    AST_BOOLEAN,
+    AST_NUMBER,
+    AST_UNKNOWN
 };
-//typedef struct ParseTree ParseTree;
+
+struct ParseTree {
+    enum NodeType type;
+    int dummy; // TODO(plesslie): remove
+};
+typedef struct ParseTree ParseTree;
+
+int g_parse_accepted = 0;
 
 struct Token {
     int value;
 };
+typedef struct Token Token;
 
-}
+} // ~include
 
 %parse_accept {
-    printf("parsing succeeded!\n");
+    //printf("parsing succeeded!\n");
+    g_parse_accepted = 1;
 }
 
 %parse_failure {
-    printf("parsing failed!\n");
+    //printf("parsing failed!\n");
+    g_parse_accepted = 0;
+    assert(0);
 }
 
 %stack_overflow {
-    printf("stack overflow\n");
+    //printf("stack overflow\n");
+    g_parse_accepted = 0;
+    assert(0);
 }
 
 %syntax_error {
-    printf("syntax error\n");
+    //printf("syntax error\n");
+    g_parse_accepted = 0;
+    assert(0);
 }
 
 %token_prefix TK_
@@ -42,12 +61,5 @@ struct Token {
 %token_type {struct Token*}
 // %type expr {Expr*}
 
-%left PLUS MINUS DIVIDE MULTIPLY.
-
-program ::= expr.
-
-expr ::= expr PLUS expr. { printf("Doing addition\n"); parse->dummy = 1; }
-expr ::= expr MINUS expr. { printf("Doing subtraction\n"); }
-expr ::= expr DIVIDE expr. { printf("Doing division\n"); }
-expr ::= expr MULTIPLY expr. { printf("Doing multiplication\n"); }
-expr ::= INTEGER(A). { printf("Matched integer: %d\n", A->value); }
+constant ::= BOOLEAN. { parse->type = AST_BOOLEAN; }
+constant ::= NUMBER. { parse->type = AST_NUMBER; }
--- main.c
+++ main.c
@@ -1,49 +1,93 @@
 #include <stdio.h>
-#include "lexer.h"
+//#include "lexer.h"
 #include "gram.h"
 #include "gram.c"
+#include <cgreen/cgreen.h>
 
-int main(int argc, char **argv) {
-    FILE *fp = fopen("tests.txt", "r");
-    if (!fp) {
-        fprintf(stderr, "Unable to open input file\n");
-        exit(EXIT_FAILURE);
-    }
+Describe(Parser);
+BeforeEach(Parser) {}
+AfterEach(Parser) {}
 
-    struct scanner_t scanner;
-    if (!scanner_create(&scanner, fp)) {
-        fprintf(stderr, "Failed to create scanner\n");
-        exit(EXIT_FAILURE);
-    }
+Ensure(Parser, boolean_is_constant) {
+    ParseTree ast;
+    Token token;
+    SchemeParser *parser = schemeParserAlloc(malloc);
 
-    if (!scanner_lex(&scanner)) {
-        printf("\nlexing failed!\n");
-    } else {
-        printf("\nsuccess!\n");
-    }
+    ast.type = AST_UNKNOWN;
+    g_parse_accepted = 0;
+    token.value = 0;
 
-    if (!scanner_destroy(&scanner)) {
-        fputs("Failed to destroy scanner\n", stderr);
-        exit(EXIT_FAILURE);
-    }
-    void *parser = schemeParserAlloc(malloc);
-    struct ParseTree ast;
-
-    struct Token token;
-    token.value = 15;
-    schemeParser(parser, TK_INTEGER, &token, &ast);
-    schemeParser(parser, TK_PLUS, &token, &ast);
-    schemeParser(parser, TK_INTEGER, &token, &ast);
+    schemeParser(parser, TK_BOOLEAN, &token, &ast);
     schemeParser(parser, 0, 0, &ast);
 
-#if 0
-    schemeParser(parser, TK_INTEGER, 15);
-    schemeParser(parser, TK_MINUS, 0);
-    schemeParser(parser, TK_INTEGER, 14);
-    schemeParser(parser, 0, 0);
-#endif
+    schemeParserFree(parser, free);
+    assert_that(g_parse_accepted == 1);
+    assert_that(ast.type == AST_BOOLEAN);
+}
+
+Ensure(Parser, number_is_constant) {
+    ParseTree ast;
+    Token token;
+    SchemeParser *parser = schemeParserAlloc(malloc);
+
+    ast.type = AST_UNKNOWN;
+    g_parse_accepted = 0;
+    token.value = 0;
+
+    schemeParser(parser, TK_NUMBER, &token, &ast);
+    schemeParser(parser, 0, 0, &ast);
 
     schemeParserFree(parser, free);
-
-    return 0;
+    assert_that(g_parse_accepted == 1);
+    assert_that(ast.type == AST_NUMBER);
 }
+
+int main(int argc, char **argv) {
+    TestSuite *suite = create_test_suite();
+    add_test_with_context(suite, Parser, boolean_is_constant);
+    return run_test_suite(suite, create_text_reporter());
+}
+
+//int main(int argc, char **argv) {
+////    FILE *fp = fopen("tests.txt", "r");
+////    if (!fp) {
+////        fprintf(stderr, "Unable to open input file\n");
+////        exit(EXIT_FAILURE);
+////    }
+////
+////    struct scanner_t scanner;
+////    if (!scanner_create(&scanner, fp)) {
+////        fprintf(stderr, "Failed to create scanner\n");
+////        exit(EXIT_FAILURE);
+////    }
+////
+////    if (!scanner_lex(&scanner)) {
+////        printf("\nlexing failed!\n");
+////    } else {
+////        printf("\nsuccess!\n");
+////    }
+////
+////    if (!scanner_destroy(&scanner)) {
+////        fputs("Failed to destroy scanner\n", stderr);
+////        exit(EXIT_FAILURE);
+////    }
+//
+//    printf("Running unit tests...\n");
+//    test_constant_boolean();
+//    test_constant_number();
+//    printf("Success!\n");
+//
+//#if 0
+//    void *parser = schemeParserAlloc(malloc);
+//    struct ParseTree ast;
+//    struct Token token;
+//    token.value = 15;
+//    schemeParser(parser, TK_INTEGER, &token, &ast);
+//    schemeParser(parser, TK_PLUS, &token, &ast);
+//    schemeParser(parser, TK_INTEGER, &token, &ast);
+//    schemeParser(parser, 0, 0, &ast);
+//    schemeParserFree(parser, free);
+//#endif
+//
+//    return 0;
+//}
